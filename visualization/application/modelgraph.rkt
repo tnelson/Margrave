b52597fa21145e4da5b5557c316220792757474d
unknown <morganquirk@.student.admin.wpi.edu>
2010-07-19 14:38:35 -0400

    Model policy decisions represented with icons. This is faked, random icons are chosen, because I don't have real model support yet.

--- visualization/application/modelgraph.rkt
+++ visualization/application/modelgraph.rkt
@@ -1,16 +1,52 @@
-#lang racket
+#lang racket/gui
 
 (provide modelgraph-node% modelgraph-edge% modelgraph% pos-modelgraph-node%
          apply-model apply-model/pos)
 
 (require "netgraph.rkt")
 
+; Result types
+(define model-result%
+  (class object%
+    (init-field
+     [bitmap null]
+     [name "UNNAMED"])
+    
+    (super-new)
+    
+    (define/public (get-bitmap) bitmap)
+    (define/public (get-name) name)))
+
+(define result-accept
+  (make-object model-result%
+    (make-object bitmap% "../images/icon_accept.png")
+    "Accept"))
+
+(define result-deny
+  (make-object model-result%
+    (make-object bitmap% "../images/icon_deny.png")
+    "Deny"))
+
+(define result-modify
+  (make-object model-result%
+    (make-object bitmap% "../images/icon_modify.png")
+    "Modify"))
+
+
+
+; Node in a modelgraph
+; Has a list of policy results (blocked, accepted, etc)
 (define modelgraph-node%
   (class netgraph-node%
     (init-field
      [results empty])
-    (super-new)))
+    (super-new)
+    
+    (define/public (get-results) results)
+    ))
 
+; Edge
+; Knows if it is active (part of the model) and if it's blocked.
 (define modelgraph-edge%
   (class netgraph-edge%
     (init-field
@@ -18,15 +54,20 @@
      [blocked #f])
     
     (define/public (is-active?) active)
-
+    
     (super-new)))
 
+; The graph
+; TODO: make sure the nodes and edges that are a part of it are modelgraph type not
+; netgraph type.
 (define modelgraph%
   (class netgraph%
     (super-new)))
 
+; Positional component mixed in
 (define pos-modelgraph-node% (pos-mixin modelgraph-node%))
 
+; Helper functions for apply-model
 (define (convert-edge e model)
   (new modelgraph-edge%
        [from (send (send e get-from) get-mnode)]
@@ -45,7 +86,7 @@
          (new pos-modelgraph-node%
               [name (send n get-name)]
               [policy (send n get-policy)]
-              [results empty]
+              [results (filter (lambda (r) (= 0 (random 2))) (list result-accept result-deny result-modify))]
               [x (send n get-x)]
               [y (send n get-y)]
               )])
