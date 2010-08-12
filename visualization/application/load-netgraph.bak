0db4f2e0778f9e4c812cf0cb895680d7faf788ee
unknown <morganquirk@.student.admin.wpi.edu>
2010-08-12 13:31:21 -0400

    macro things and loading

---
+++ visualization/application/load-netgraph.bak
@@ -0,0 +1,6 @@
+#lang racket
+
+(require "netgraph.rkt")
+
+(define (load-netgraph filename)
+  )
--- visualization/application/load-netgraph.rkt
+++ visualization/application/load-netgraph.rkt
@@ -1,6 +1,26 @@
 #lang racket
 
+(provide topology load-topology)
 (require "netgraph.rkt")
 
-(define (load-netgraph filename)
-  )
+(define-syntax topology
+  (syntax-rules ()
+    [(topology
+      ((node N1 [key val] ...) ...)
+      ((edge A1 A2) ...))
+     (let ([ng (new netgraph% )]
+           [N1 (new pos-netgraph-node% [key val] ...)] ...)
+       (begin
+         (begin
+           (send ng add-node! N1)
+           ...
+           )
+         (begin
+           (send ng add-edge! A1 A2)
+           ...
+           )
+         ng
+         ))]))
+
+(define (load-topology filename)
+  (eval (with-input-from-file filename read)))
