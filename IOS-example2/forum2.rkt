69ba6ba754564f0d51d2ced607266329d03848d8
tnelson <tn@cs.wpi.edu>
2010-08-16 17:39:12 -0400

    Added BAZ router config for forum example 2

--- IOS-example2/forum2.rkt
+++ IOS-example2/forum2.rkt
@@ -3,11 +3,7 @@
 (require (file "../margrave.rkt")
          (file "../margrave-ios.rkt"))
 
-; IMPORTANT! --->
-; Things that need bugfixing for the paper are marked with TODO.
-
-
-(define oldvector "(ahostname, entry-interface, 
+(define tasvector "(ahostname, entry-interface, 
         src-addr-in, src-addr-out, 
         dest-addr-in, dest-addr-out, 
         protocol, message,
@@ -15,7 +11,7 @@
         dest-port-in, dest-port-out, 
         length, next-hop, exit-interface)")
 
-(define vector "(ahostname, entry-interface, 
+(define bazvector "(ahostname, entry-interface, 
         src-addr-in, src-addr_, src-addr-out, 
         dest-addr-in, dest-addr_, dest-addr-out, 
         protocol, message,
@@ -35,89 +31,86 @@
   ; Pass path of the engine files: 1 level up from here.
   (start-margrave-engine (build-path (current-directory) 'up))
   
-  (with-handlers ((exn:fail? (lambda (v) 
-                               (printf "Racket produced a failure exception.~n Stopping the Margrave engine.~n")
-                               (stop-margrave-engine)
-                               (raise v))))
-    
-    ; Load all the policies 
-    ; InboundACL -> InboundACL1, InboundACL2, InboundACL3 respectively.
-    (load-ios-policies (build-path (current-directory) "config") "" "1")
-    (load-ios-policies (build-path (current-directory) "config-revised") "" "2")
-    
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-    ; Version 1
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-    
-    (printf "~n---------------1--------------~n----------------------------~n")
-    
-    
-    (mtext (string-append "EXPLORE
+  ; Load all the policies 
+  (load-ios-policies (build-path (current-directory) "tasconfig") "tas" "1")
+  (load-ios-policies (build-path (current-directory) "tasconfig-revised") "tas" "2")
+  (load-ios-policies (build-path (current-directory) "bazconfig") "baz" "1")
+;  (load-ios-policies (build-path (current-directory) "bazconfig-revised") "baz" "2")
+  
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ; Version 1
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  
+  (printf "~n---------------1--------------~n----------------------------~n")
+  
+  
+  (mtext (string-append "EXPLORE
 NOT ip-192-168-2-0/ip-255-255-255-0(src-addr-in) AND
 FastEthernet0(entry-interface) AND
 prot-TCP(protocol) AND
 port-80(src-port-in) AND
 routed-packets1" vector
-" TUPLING"))
-
-    (mtext "IS POSSIBLE? 0")  
-    
-    (mtext "GET ONE 0")
-    (mtext "GET NEXT 0")
-    
-    ;; TODO shouldn't we have a keyword that's syntactic sugar for "all subsorts of..."? 
-    ;; TODO no way to show populated "other"... without that, this isn't a very interesting show populated?
-    ;;   (but abstractness is expensive. can we get "other" here without an explicit sort and abstractness?)
-    ; !!! TODO tupling isn't shown in the paper
-   
-    (mtext (string-append "SHOW POPULATED 0 port-80(dest-port-in)" 
-                         ", port-20(dest-port-in)" 
-                         ", port-21(dest-port-in)" 
-                         ", port-23(dest-port-in)" 
-                         ", port-3389(dest-port-in)"))
-    
-
-    (mtext (string-append "EXPLORE
+                 " TUPLING"))
+  
+  (mtext "IS POSSIBLE? 0")  
+  
+  (mtext "GET ONE 0")
+  (mtext "GET NEXT 0")
+  
+  ;; TODO shouldn't we have a keyword that's syntactic sugar for "all subsorts of..."? 
+  ;; TODO no way to show populated "other"... without that, this isn't a very interesting show populated?
+  ;;   (but abstractness is expensive. can we get "other" here without an explicit sort and abstractness?)
+  ; !!! TODO tupling isn't shown in the paper
+  
+  (mtext (string-append "SHOW POPULATED 0 port-80(dest-port-in)" 
+                        ", port-20(dest-port-in)" 
+                        ", port-21(dest-port-in)" 
+                        ", port-23(dest-port-in)" 
+                        ", port-3389(dest-port-in)"))
+  
+  
+  (mtext (string-append "EXPLORE
 NOT ip-192-168-2-0/ip-255-255-255-0(src-addr-in) AND
 FastEthernet0(entry-interface) AND
 prot-TCP(protocol) AND
 port-80(src-port-in) AND
 routed-packets1" vector " AND " ; *1*
-"NOT port-80(dest-port-in) AND
+                 "NOT port-80(dest-port-in) AND
  NOT port-20(dest-port-in) AND
  NOT port-21(dest-port-in) AND
  NOT port-23(dest-port-in) AND
  NOT port-3389(dest-port-in)"
-" TUPLING"))
-
-    (mtext "IS POSSIBLE? 0")    
-    
-                         
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-    ; Version 2
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-    
-    (printf "~n---------------2--------------~n----------------------------~n")
-    
-    ; TODO: No "other" or "unreferenced" keyword makes us enumerate those 5 ports.
-    
-    (mtext (string-append "EXPLORE
+                 " TUPLING"))
+  
+  (mtext "IS POSSIBLE? 0")    
+  
+  
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ; Version 2
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  
+  (printf "~n---------------2--------------~n----------------------------~n")
+  
+  ; TODO: No "other" or "unreferenced" keyword makes us enumerate those 5 ports.
+  
+  (mtext (string-append "EXPLORE
 NOT ip-192-168-2-0/ip-255-255-255-0(src-addr-in) AND
 FastEthernet0(entry-interface) AND
 prot-TCP(protocol) AND
 port-80(src-port-in) AND
 routed-packets2" vector " AND " ; *2*
-"NOT port-80(dest-port-in) AND
+                 "NOT port-80(dest-port-in) AND
  NOT port-20(dest-port-in) AND
  NOT port-21(dest-port-in) AND
  NOT port-23(dest-port-in) AND
  NOT port-3389(dest-port-in)"
-" TUPLING"))
-
-    (mtext "IS POSSIBLE? 0")
-    
-   
-    
-    
-    
-  (stop-margrave-engine)))
+                 " TUPLING"))
+  
+  (mtext "IS POSSIBLE? 0")
+  
+  
+  
+  
+  
+  ;(stop-margrave-engine)
+  )
