b0958ad8b56468e488d97e4a4c04ab5c74a5ccb9
Timothy Nelson <tn@cs.wpi.edu>
2010-09-24 22:21:38 -0400

    handle drracket:submit-predicate so that ;<enter>, #<enter>, etc. won't cause weird behavior in the interactions window. Now hitting enter immediately sends to the reader.

--- margrave/language-info.rkt
+++ margrave/language-info.rkt
@@ -9,4 +9,13 @@
       [(configure-runtime)    
        ; Note: LIST of vectors... allows for multiple config functions to be run
       '(#(margrave/runtime-config configure #f))]
+      [(drracket:submit-predicate)
+       margrave-repl-submit-predicate]
       [else default])))
+
+
+; **********************************************************
+
+; ALWAYS pass to Margrave's reader
+(define (margrave-repl-submit-predicate interactions-port only-whitespace)
+  #t)
