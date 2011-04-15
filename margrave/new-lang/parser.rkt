efdc5366b73c563bfee924725ed486a746576709
tnelson <tn@cs.wpi.edu>
2011-04-15 10:27:03 -0400

    parser work

--- margrave/new-lang/parser.rkt
+++ margrave/new-lang/parser.rkt
@@ -31,6 +31,21 @@
 (provide parse)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Helpers
+
+(define (started-with token-list)  
+  (define rev-tokens (reverse (map position-token-token token-history)))  
+  (and (>= (length rev-tokens) (length token-list))
+       (equal? token-list (take rev-tokens (length token-list)))))
+
+(define (started-with-identifier val)  
+  (unless (empty? token-history)
+    (define the-tok (position-token-token (last token-history)))  
+    ;(printf "~a~n" the-tok)
+    (and (token? the-tok)
+         (equal? (token-value the-tok) val ))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;Taken from /collects/parser-tools/examples/read.ss
 (define stx-for-original-property (read-syntax #f (open-input-string "original")))
 
@@ -136,75 +151,69 @@
        (tokens empty-terminals terminals)
        
        ;**************************************************   
-       (error (lambda (tok-ok? token-name token-value start-pos end-pos)                 
-                (define rev-tokens (reverse (map position-token-token token-history)))
-                
-                ;(printf "History: ~a~n" rev-tokens)
-                
-                (define first-token (if (> (length rev-tokens) 0)
-                                        (first rev-tokens)
-                                        #f))
-                (define second-token (if (> (length rev-tokens) 1)
-                                         (second rev-tokens)
-                                         #f))                  
+       (error (lambda (tok-ok? token-name token-value start-pos end-pos) 
+                ; Token-history is reversed here.
+                ;(printf "History: ~a~n" token-history)
                 
                 ; Can we guess at the kind of command they were trying to use?
                 ; customize an error message depending on history:
                 (define error-message
-                  (cond [(empty? rev-tokens)
+                  (cond [(empty? token-history)
                          "Please enter a Margrave command."]
-                        [(token? first-token) ; token? is not true of empty-tokens like all the correct command keywords.
-                         "Please enter a valid Margrave command."]
-                       
-                        [(equal? first-token 'RENAME)
-                         "Please use the following: RENAME <old name> <new name>"]                        
-                                              
-                        [(and (> (length rev-tokens) 1)
-                              (equal? first-token 'LOAD)
-                              (equal? second-token 'IOS))
-                         "LOAD IOS must be followed by a configuration file name."]
-                        [(and (> (length rev-tokens) 1)
-                              (equal? first-token 'LOAD)
-                              (equal? second-token 'POLICY))
-                         "LOAD POLICY must be followed by a .p file name."]
-                        [(and (equal? first-token 'LOAD)
-                              (equal? second-token 'XACML))
-                         "LOAD IOS must be followed by an XACML policy file name."]
-                        [(and (equal? first-token 'LOAD)
-                              (equal? second-token 'SQS))
-                         "LOAD SQS must be followed by an Amazon SQS JSON file name."]
-                        [(equal? first-token 'LOAD)
-                         "LOAD what? (LOAD POLICY to load a .p file, LOAD IOS to load a Cisco IOS configuration, etc.)" ]
+                        ;[(token? first-token) ; token? is not true of empty-tokens like all the correct command keywords.
+                        ; "Please enter a valid Margrave command."]
+                                                                     
+                        [(started-with '(LOAD IOS))
+                         "To load an IOS configuration, use #LOAD IOS <file name>. Other options are given in the documentation."]
+                                                
+                        [(started-with-identifier 'LOAD)
+                         "The #LOAD directive must be prefixed by a # symbol."]
                         
-                        [(equal? first-token 'COUNT)
-                         "COUNT is a stand-alone command."]
+                        [(started-with '(LOAD POLICY))
+                         "To load a .p policy file, use #LOAD POLICY <desired name> = <file name>"]
                         
-                        [(and (equal? first-token 'SHOW)
-                              (equal? second-token 'REALIZED))
-                         "That was not a valid SHOW REALIZED command."]
-                        [(and (equal? first-token 'SHOW)
-                              (equal? second-token 'UNREALIZED))
-                         "That was not a valid SHOW UNREALIZED command."]
+                        [(started-with '(LOAD XACML))
+                         "To load an XACML policy file, use #LOAD XACML <desired name> = <file name>"]
+                        
+                        [(started-with '(LOAD SQS))
+                         "To load an Amazon SQS policy file, use #LOAD SQS <desired name> = <file name>"]
+                        
+                        [(started-with '(LOAD))
+                         "Load what? (#LOAD POLICY to load a .p file, #LOAD IOS to load a Cisco IOS configuration, etc.)" ]
+                        
+                        [(started-with '(LET))
+                         (format "To bind a formula context, use LET <name>[<variable-ordering>] BE <condition>. 
+Margrave did not understand the condition or options given around \"~a\"." 
+                                 (if token-value
+                                     token-value
+                                     token-name))]
                         
                         
-                        [(equal? first-token 'SHOW)
-                         "SHOW must be followed by a mode. Which SHOW did you intend to use? (SHOW ONE, SHOW ALL, SHOW NEXT, SHOW CEILING...)"]
-                        [(equal? first-token 'GET)
-                         "GET must be followed by a mode. Which GET did you intend to use? (GET ONE, GET ALL, GET NEXT, GET RULES IN...)"]
-                        
-                        [(and (equal? first-token 'IS)
-                              (equal? second-token 'POSSIBLEQMARK))
-                         "IS POSSIBLE? is a standalone command."]
-                        [(equal? first-token 'IS)
-                         "Did you mean IS POSSIBLE? ?"]
-                        
-                        [(equal? first-token 'DEFVEC)
-                         "A DEFVEC command is of the form DEFVEC <vecid> x, y, z..."]
-                        
-                        [(equal? first-token 'EXPLORE)
-                         (format "EXPLORE must be followed by a boolean condition. Margrave did not understand the condition or options given around \"~a\"." (if token-value
-                                                                                                                                                                  token-value
-                                                                                                                                                                  token-name))]
+;                        [(equal? first-token 'COUNT)
+;                         "COUNT is a stand-alone command."]
+;                        
+;                        [(and (equal? first-token 'SHOW)
+;                              (equal? second-token 'REALIZED))
+;                         "That was not a valid SHOW REALIZED command."]
+;                        [(and (equal? first-token 'SHOW)
+;                              (equal? second-token 'UNREALIZED))
+;                         "That was not a valid SHOW UNREALIZED command."]
+;                        
+;                        
+;                        [(equal? first-token 'SHOW)
+;                         "SHOW must be followed by a mode. Which SHOW did you intend to use? (SHOW ONE, SHOW ALL, SHOW NEXT, SHOW CEILING...)"]
+;                        [(equal? first-token 'GET)
+;                         "GET must be followed by a mode. Which GET did you intend to use? (GET ONE, GET ALL, GET NEXT, GET RULES IN...)"]
+;                        
+;                        [(and (equal? first-token 'IS)
+;                              (equal? second-token 'POSSIBLEQMARK))
+;                         "IS POSSIBLE? is a standalone command."]
+;                        [(equal? first-token 'IS)
+;                         "Did you mean IS POSSIBLE? ?"]
+;                        
+;                        [(equal? first-token 'DEFVEC)
+;                         "A DEFVEC command is of the form DEFVEC <vecid> x, y, z..."]
+;                        
                         
                         ; Last resort
                         [else (format "Margrave did not understand that command.")]))
@@ -545,7 +554,9 @@
 ; *************************************************
 ; Tests
 
-(define (parse-s s)
+(define (test-parse-s s)
   (define in (open-input-string s))
   (port-count-lines! in)
   ((parse "test param to parse-s") (lambda () (lex in))))
+
+; (parse-s "let F[x, y, z] be P.C.Foo(x, y) and not Bar(z);")
