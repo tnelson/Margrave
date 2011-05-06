8c4c7d299a3be8e5402bc23c6f80bc63492c623c
Timothy Nelson <tn@cs.wpi.edu>
2011-05-06 15:31:12 -0400

    .

--- margrave/examples/conference/conference-script.rkt
+++ margrave/examples/conference/conference-script.rkt
@@ -1,7 +1,7 @@
 #lang margrave
 
-#load policy PConf = "*margrave*/examples/conference/conference.p";
+#load policy PConf = 
+"*margrave*/examples/conference/conference.p";
 
-// Who can read scores?
 let Q1[s: Subject, a: Action, r: Resource] be
   PConf.permit(s, a, r) and a : SubmitReview;
